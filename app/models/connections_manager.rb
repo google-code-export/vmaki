1f2499ade6be6f1576f1daafb7c3e8688b51651c
unabatede <unabatede@874bd674-5980-11de-8d7d-01d876f1962d>
2009-06-17 09:25:18 +0000

    implemented NFS share mounting

--- app/models/connections_manager.rb
+++ app/models/connections_manager.rb
@@ -1,6 +1,7 @@
 require "libvirt"
 require 'host'
 require 'singleton'
+require 'net/ssh'
 require 'ssh_key'
 
 class ConnectionsManager
@@ -16,8 +17,11 @@ class ConnectionsManager
 		if password_less_connection
 			connection_string = "#{host.connection_type}#{host.username}@#{host.name}"
 			puts "connecting to: #{connection_string}"
-			# put SSH Connection stuff here!
 			conn = Libvirt::open(connection_string)
+
+      # mount NFS share
+      mount_nfs(host)
+
 			Dblogger.log("Debug", "system", "Host", conn.capabilities)
 						
 			@connections[host.name] = { :host => host.clone, :conn => conn}
@@ -32,5 +36,27 @@ class ConnectionsManager
 
 	def get(name)
 		return @connections[name]
+	end
+
+  def mount_nfs(host)
+    Net::SSH.start(host.name, host.username, :auth_methods => "publickey", :timeout => 2) do |ssh|
+      puts "Unmounting existing NFS Share"
+      puts ssh.exec!("umount /mnt/tmp")
+      puts "Mounting NFS Share from Management Node"
+      puts ssh.exec!("mount #{local_ip}:/isos /mnt/tmp")
+    end
+  end
+
+  def local_ip
+		# turn off reverse DNS resolution temporarily
+		orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+
+		UDPSocket.open do |s|
+			# One of Google's IPs. It is NOT being used in any way though, since no real connection is opened
+			s.connect '64.233.187.99', 1
+			s.addr.last
+		end
+	ensure
+		Socket.do_not_reverse_lookup = orig
 	end
 end
