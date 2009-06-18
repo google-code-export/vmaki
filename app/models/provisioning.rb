71ba7c79d36e0dc0034b1b97d7712ea433a55c8e
unabatede <unabatede@874bd674-5980-11de-8d7d-01d876f1962d>
2009-06-18 23:11:01 +0000

    Added the first part of the snapshotting functionality

--- app/models/provisioning.rb
+++ app/models/provisioning.rb
@@ -40,58 +40,6 @@ class Provisioning
     # synonym for newline
     endl = "\n"
 
-    #inittab = "# /etc/inittab: init(8) configuration." + endl
-    #inittab << "# $Id: inittab,v 1.91 2002/01/25 13:35:21 miquels Exp $" + endl
-    #inittab << endl
-    #inittab << "# The default runlevel." + endl
-    #inittab << "id:2:initdefault:" + endl
-    #inittab << endl
-    #inittab << "# Boot-time system configuration/initialization script." + endl
-    #inittab << "# This is run first except when booting in emergency (-b) mode." + endl
-    #inittab << "si::sysinit:/etc/init.d/rcS" + endl
-    #inittab << endl
-    #inittab << "# What to do in single-user mode." + endl
-    #inittab << "~~:S:wait:/sbin/sulogin" + endl
-    #inittab << endl
-    #inittab << "# /etc/init.d executes the S and K scripts upon change" + endl
-    #inittab << "# of runlevel." + endl
-    #inittab << "#" + endl
-    #inittab << "# Runlevel 0 is halt." + endl
-    #inittab << "# Runlevel 1 is single-user." + endl
-    #inittab << "# Runlevels 2-5 are multi-user." + endl
-    #inittab << "# Runlevel 6 is reboot." + endl
-    #inittab << endl
-    #inittab << "l0:0:wait:/etc/init.d/rc 0" + endl
-    #inittab << "l1:1:wait:/etc/init.d/rc 1" + endl
-    #inittab << "l2:2:wait:/etc/init.d/rc 2" + endl
-    #inittab << "l3:3:wait:/etc/init.d/rc 3" + endl
-    #inittab << "l4:4:wait:/etc/init.d/rc 4" + endl
-    #inittab << "l5:5:wait:/etc/init.d/rc 5" + endl
-    #inittab << "l6:6:wait:/etc/init.d/rc 6" + endl
-    #inittab << "# Normally not reached, but fallthrough in case of emergency." + endl
-    #inittab << "z6:6:respawn:/sbin/sulogin" + endl
-    #inittab << endl
-    #inittab << "# What to do when CTRL-ALT-DEL is pressed." + endl
-    #inittab << "ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now" + endl
-    #inittab << endl
-    #inittab << "# What to do when the power fails/returns." + endl
-    #inittab << "pf::powerwait:/etc/init.d/powerfail start" + endl
-    #inittab << "pn::powerfailnow:/etc/init.d/powerfail now" + endl
-    #inittab << "po::powerokwait:/etc/init.d/powerfail stop" + endl
-    #inittab << endl
-    #inittab << "# /sbin/getty invocations for the runlevels." + endl
-    #inittab << "#" + endl
-    #inittab << "# The 'id' field MUST be the same as the last" + endl
-    #inittab << "# characters of the device (after 'tty')." + endl
-    #inittab << "#" + endl
-    #inittab << "# Format:" + endl
-    #inittab << "#  <id>:<runlevels>:<action>:<process>" + endl
-    #inittab << "#" + endl
-    #inittab << "# Note that on most Debian systems tty7 is used by the X Window System," + endl
-    #inittab << "# so if you want to add more getty's go ahead but skip tty7 if you run X." + endl
-    #inittab << "#" + endl
-    #inittab << "1:2345:respawn:/sbin/getty 38400 tty1" + endl
-
     fstab = "/dev/sda1  none  swap sw                 0 0" + endl
     fstab << "/dev/sda2 /     ext3 errors=remount-ro  0 1" + endl
     fstab << "proc      /proc proc defaults           0 0" + endl
@@ -104,17 +52,10 @@ class Provisioning
     interfaces << "iface lo inet loopback" + endl
     interfaces << "iface eth0 inet dhcp" + endl
 
-		hostname = vmname
+		#hostname = vmname
 
     puts "chroot"
     Net::SFTP.start(@hostname, @username, :auth_methods => "publickey", :timeout => Constants::SSH_Timeout) do |sftp|
-      # write inittab
-      #sftp.file.open("#{basedir}/etc/inittab", IO::WRONLY|IO::CREAT) do |file|
-      #  inittab.each do |data|
-      #    file.puts data
-      #  end
-      #end
-
       # write fstab
 			puts "fstab"
       sftp.file.open("#{basedir}/etc/fstab", IO::WRONLY|IO::CREAT) do |file|
--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,14 +1,46 @@
+require "constants"
+
 class Snapshot < ActiveRecord::Base
 	validates_uniqueness_of :name
 
 	belongs_to :vm
 
-	before_create :set_name
+	before_create :manage_create
 
-	def set_name
+	before_update :manage_update
+
+	def manage_create
 		vm = Vm.find_by_id(self.vm_id)
 		snapshots = Snapshot.find(:all, :conditions => {:vm_id => self.vm_id} )
 		snapshot_nr = snapshots.size + 1
 		self.name = "#{vm.name}-snapshot-#{snapshot_nr}"
+
+		volume = Volume.find_by_id(vm.rootvolume_id)
+		pool = Pool.find_by_id(volume.pool_id)
+		
+		# establish an SSH connection
+		host = Host.find_by_id(vm.host_id)
+		Net::SSH.start(host.name, host.username, :auth_methods => "publickey", :timeout => Constants::SSH_Timeout) do |ssh|
+			snapshot_filename = "#{Constants::SNAPSHOTS_PATH}/#{self.name}.img"
+			# first make sure the snapshot folder exists
+			puts "creating snapshot folder"
+			puts ssh.exec!("mkdir -p #{snapshot_filename}")
+			# create the LVM snapshot
+			puts "creating snapshot"
+			puts ssh.exec!("lvcreate -L #{Constants::LVM_SNAPSHOT_SIZE} -s -n #{self.name} #{pool.id}/#{volume.name}")
+			puts "copying snapshot"
+			puts ssh.exec!("dd if=#{pool.id}/#{self.name} of=#{snapshot_filename}")
+			
+			# retrieve filesize of the created .img file
+			results = ssh.exec!("ls -sh #{snapshot_filename}").split(" ")
+			filesize = results[0]
+			self.size = filesize
+		end
+	end
+
+	def manage_update
+		if self.changed? && (self.status != "creating" || self.status != "restoring")
+
+		end
 	end
 end
