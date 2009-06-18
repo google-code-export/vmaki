4e88865e1b270519c9265b268da2dc7e19b60894
unabatede <unabatede@874bd674-5980-11de-8d7d-01d876f1962d>
2009-06-18 10:04:37 +0000

    Added modified version of ruby-libvirt to support virDomainAttachDevice & virDomainDetachDevice Implemented run-time attaching of drives (not possible with PV guests, though!) Removed obsolete folder libvirt-tests

--- trunk/libvirt-tests/main.rb
+++
@@ -1,2 +0,0 @@
-#TODO #1: Find out why some threads don't terminate and why some things have to be run independently
-
--- trunk/libvirt-tests/unit-tests/ts_suite.rb
+++
@@ -1,5 +0,0 @@
-require 'unit-tests/test_open_connection'
-require 'unit-tests/test_pool'
-require 'unit-tests/test_volume'
-require 'unit-tests/test_v_m'
-require 'unit-tests/test_close_connection'
